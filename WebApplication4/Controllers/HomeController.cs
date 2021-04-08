1026120277e09cb51e17dc8109b7b0bb2e808531
adhilahamed333 <37613096+adhilahamed333@users.noreply.github.com>
2021-04-08 19:12:59 +0530

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //if (Request["txt"] != null)
+            //{
+            //    if (Request["txt"][Request["txt"].Length - 1] == '+' || Request["txt"][Request["txt"].Length - 1] == '-' || Request["txt"][Request["txt"].Length - 1] == '*' || Request["txt"][Request["txt"].Length - 1] == '/')
+            //    {
+            //        ViewBag.flag = 0;
+            //        ViewBag.result = Request["txt"];
+            //    }
+            //    else
+            //    {
+            //        ViewBag.result = GetResult(Request["txt"]);
+            //        ViewBag.flag = 1;
+            //    }
+            //}
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Evaluate(String txt)
+        {
+            String str = txt;
+            List<char> symbleList = new List<char>();
+            char[] charSymble = { '+', '-', '*', '/' };
+            string[] st = str.Split(charSymble);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+                {
+                    symbleList.Add(str[i]);
+                }
+            }
+            double result = Convert.ToDouble(st[0]);
+            for (int i = 1; i < st.Length; i++)
+            {
+                double num = Convert.ToDouble(st[i]);
+                int j = i - 1;
+                switch (symbleList[j])
+                {
+                    case '+':
+                        result = result + num;
+                        break;
+                    case '-':
+                        result = result - num;
+                        break;
+                    case '*':
+                        result = result * num;
+                        break;
+                    case '/':
+                        result = result / num;
+                        break;
+                    default:
+                        result = 0.0;
+                        break;
+                }
+            }
+
+            ViewBag.Result = result.ToString();
+
+            return View();
+            
+        }
+
+        //public string GetResult(string str)
+        //{
+        //    List<char> symbleList = new List<char>();
+        //    char[] charSymble = { '+', '-', '*', '/' };
+        //    string[] st = str.Split(charSymble);
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+        //        {
+        //            symbleList.Add(str[i]);
+        //        }
+        //    }
+        //    double result = Convert.ToDouble(st[0]);
+        //    for (int i = 1; i < st.Length; i++)
+        //    {
+        //        double num = Convert.ToDouble(st[i]);
+        //        int j = i - 1;
+        //        switch (symbleList[j])
+        //        {
+        //            case '+':
+        //                result = result + num;
+        //                break;
+        //            case '-':
+        //                result = result - num;
+        //                break;
+        //            case '*':
+        //                result = result * num;
+        //                break;
+        //            case '/':
+        //                result = result / num;
+        //                break;
+        //            default:
+        //                result = 0.0;
+        //                break;
+        //        }
+        //    }
+        //    return result.ToString();
+        //}
+
+
+    }
+}
---
+++ WebApplication4/Scripts/Compute.js
@@ -0,0 +1,182 @@
+﻿    $(function()
+    {
+        $("#txt").keypress(function (e) {
+            if (String.fromCharCode(e.keyCode).match(/[^0-9+-/*]/g)) return false;
+        });
+
+        $("#b0").click(function() {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("0");
+            } else
+            {
+        $("#txt").val(x + "0")
+    }
+        });
+
+        $("#b1").click(function()
+        {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("1");
+            } else
+            {
+        $("#txt").val(x + "1")
+    }
+        });
+
+        $("#b2").click(function()
+        {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("2");
+            } else
+            {
+        $("#txt").val(x + "2")
+    }
+        });
+
+        $("#b3").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("3");
+            } else
+            {
+        $("#txt").val(x + "3")
+    }
+        });
+
+        $("#b4").click(function()
+         {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("4");
+            } else
+            {
+        $("#txt").val(x + "4")
+    }
+        });
+
+        $("#b5").click(function()
+         {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("5");
+            } else
+            {
+        $("#txt").val(x + "5")
+    }
+        });
+
+        $("#b6").click(function()
+         {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("6");
+            } else
+            {
+        $("#txt").val(x + "6")
+    }
+        });
+
+        $("#b7").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+
+           {
+        $("#txt").val("7");
+            } else
+            {
+        $("#txt").val(x + "7")
+    }
+        });
+
+        $("#b8").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("8");
+            } else
+            {
+        $("#txt").val(x + "8")
+    }
+        });
+
+        $("#b9").click(function()
+         {
+            var x = document.getElementById("txt").value;
+            if (x == "0")
+            {
+        $("#txt").val("9");
+            } else
+            {
+        $("#txt").val(x + "9")
+    }
+        });
+
+        
+        $("#div").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            var y = x.slice(-1)
+            if (y == "/" || y == "+" || y == "*" || y == "-") { } else {
+        $("#txt").val(x + "/")
+    }
+        });
+
+        $("#multy").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            var y = x.slice(-1)
+            if (y == "/" || y == "+" || y == "*" || y == "-") { } else {
+        $("#txt").val(x + "*")
+    }
+        });
+
+        $("#sub").click(function()
+          {
+            var x = document.getElementById("txt").value;
+            var y = x.slice(-1)
+            if (y == "/" || y == "+" || y == "*" || y == "-") { } else {
+        $("#txt").val(x + "-")
+    }
+        });
+
+        $("#add").click(function()
+         {
+            var x = document.getElementById("txt").value;
+            var y = x.slice(-1)
+            if (y == "/" || y == "+" || y == "*" || y == "-") { } else {
+        $("#txt").val(x + "+")
+    }
+        });
+
+        $("#eq").click(function () {
+            var x = document.getElementById("txt").value;
+            var y = x.slice(-1)
+            console.log(eval(x));
+
+        //    $.ajax({
+        //url: '/home/evaluate',
+        //        type: "post",
+        //        data: {'txt': y},
+        //        success: function (msg) {
+        //$('#txt').html(msg);
+        //        }
+        //    }).error(function () {
+        //$('#txt').html('error');
+        //    }
+        //    );
+
+        });
+    });
